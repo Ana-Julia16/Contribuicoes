44fd3e080d2a01bd5d5466cfd9944c9bcd051d20
Ana A. Ramos <123672228+Ana-Julia16@users.noreply.github.com>
2023-09-13 20:52:52 -0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: FAMILIA RAMOS
+ * Date: 29/03/2023
+ * Time: 16:42
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Contribuições
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(2, 41);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(121, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(2, 179);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(121, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"Gerência",
+									"Vendas",
+									"Estoque",
+									"Contabilidade",
+									"RH"});
+			this.comboBox1.Location = new System.Drawing.Point(2, 111);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(2, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Nome:";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(2, 82);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Setor:";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(2, 150);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Contribuição:";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(204, 3);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(200, 292);
+			this.richTextBox1.TabIndex = 6;
+			this.richTextBox1.Text = "";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(542, 5);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(200, 290);
+			this.listBox1.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(204, 302);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(200, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Total:";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(542, 302);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(200, 23);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Média:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(152, 139);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(35, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = ">>";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(433, 173);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 11;
+			this.button2.Text = "Listar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(92, 297);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "Dados";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.comboBox1);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Location = new System.Drawing.Point(10, 28);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(142, 234);
+			this.panel1.TabIndex = 13;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+									"Gerência",
+									"Vendas",
+									"Estoque",
+									"Contabilidade",
+									"RH"});
+			this.comboBox2.Location = new System.Drawing.Point(410, 139);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 14;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(754, 340);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.richTextBox1);
+			this.Name = "MainForm";
+			this.Text = "Contribuições";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
--- MainForm.cs
+++ MainForm.cs
@@ -1,113 +1,119 @@
-*
-* Created by SharpDevelop.
-* User: Rm20222930058
-* Date: 29/03/2023
-* Time: 10:30* 
-* To change this template use Tools | Options | Coding | Edit Standard Headers.
-*/
-usingSystem;
-usingSystem.Collections.Generic;usingSystem.Drawing;
-usingSystem.Windows.Forms;
-namespaceContribuições
+﻿/*
+ * Created by SharpDevelop.
+ * User: Rm20222930058
+ * Date: 29/03/2023
+ * Time: 10:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Contribuições
 {
-  publicpartialclassMainForm : Form
-  {
-    publicMainForm()
-    {
-      InitializeComponent();
-    }
-    voidMainFormLoad(objectsender, EventArgs e)
-    {
-      try
-      {
-        richTextBox1.LoadFile("contribuições.txt", RichTextBoxStreamType.PlainText);
-      }catch{
-      
-      }
-    }
-    float[] b = newfloat[1];
-    string[] a = newstring[3];
-    float[]soma = newfloat[1];
-    floatp = 0;float[]valorG = newfloat[1];
-    float[]valorV = newfloat[1];
-    float[]valorE = newfloat[1];
-    float[]valorC = newfloat[1];
-    float[]valorR = newfloat[1];
-    voidButton1Click(objectsender, EventArgs e)
-    {
-      if(textBox1.Text != ""&& comboBox1.Text != ""&& textBox2.Text != "")
-      {
-        a[0] = textBox1.Text;
-        a[1] = comboBox1.Text;
-        b[0] = float.Parse(textBox2.Text);
-        richTextBox1.Text += a[0] + "\t"+ a[1] + "\t"+ b[0].ToString() + "\n";
-        richTextBox1.SaveFile("c4ontribuições.txt", RichTextBoxStreamType.PlainText);
-        p++;
-        soma[0] += b[0];
-        if(a[1]=="Gerência")
-        {
-          valorG[0] += b[0];
-        }
-        if(a[1]=="Vendas")
-        {
-          valorV[0] += b[0];
-        }
-        if(a[1]=="Estoque")
-        {
-          valorE[0] += b[0];
-        }
-        if(a[1]=="Contabilidade")
-        {
-          valorC[0] += b[0];
-        }
-        if(a[1]=="RH")
-        {
-          valorR[0] += b[0];
-        }
-        MessageBox.Show("Registro gravado com sucesso!");
-        textBox1.Clear();
-        comboBox1.Text = "";
-        textBox2.Clear();
-        textBox1.Focus();
-      }
-      else
-        MessageBox.Show("Preencha todos os campos!");
-    }
-    voidButton2Click(objectsender, EventArgs e)
-    {
-      string[] dados = richTextBox1.Text.Split('\t');
-      if(comboBox2.SelectedIndex.Equals(0))
-      {
-        listBox1.Items.Add("Gerência: "+ valorG[0]);
-      }
-      if(comboBox2.SelectedIndex.Equals(1)) 
-      {
-        listBox1.Items.Add("Vendas: "+ valorV[0]);
-      }
-      if(comboBox2.SelectedIndex.Equals(2))
-      {
-        listBox1.Items.Add("Estoque: "+ valorE[0]);
-      }
-      if(comboBox2.SelectedIndex.Equals(3))
-      {
-        listBox1.Items.Add("Contabilidadde: "+ valorC[0]);
-      }
-      if(comboBox2.SelectedIndex.Equals(4))
-      {
-        listBox1.Items.Add("RH: "+ valorR[0]);
-      }
-    }
-    voidButton3Click(objectsender, EventArgs e)
-    {
-      float soma = 0;
-      for(int i = 0; i < richTextBox1.Lines.Length-1; i++)
-      {
-        string[] dados = richTextBox1.Lines[i].Split('\t');
-        soma += float.Parse(dados[2]);
-      }
-      label4.Text = "Total: "+ soma.ToString("R$ #,###,##0,00");
-      floatmedia = soma / (richTextBox1.Lines.Length-1);
-      label5.Text = "Média: "+ media.ToString("R$,#,###,##0,00");
-    }
-  }
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        void MainFormLoad(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.LoadFile("contribuições.txt", RichTextBoxStreamType.PlainText);
+            }catch
+            {
+
+            }
+        }
+        float[] b = new float[1];
+        string[] a = new string[3];
+        float[]soma = new float[1];
+        float p = 0;
+        float[]valorG = new float[1];
+        float[]valorV = new float[1];
+        float[]valorE = new float[1];
+        float[]valorC = new float[1];
+        float[]valorR = new float[1];
+        void Button1Click(object sender, EventArgs e)
+        {
+        	if (textBox1.Text != "" && comboBox1.Text != "" && textBox2.Text != "")
+        	{
+	        	a[0] = textBox1.Text;
+	        	a[1] = comboBox1.Text;
+	        	b[0] = float.Parse(textBox2.Text);
+	        	richTextBox1.Text += a[0] + "\t" + a[1] + "\t" + b[0].ToString() + "\n";
+	        	richTextBox1.SaveFile("c4ontribuições.txt", RichTextBoxStreamType.PlainText);
+	        	p++;
+	        	soma[0] += b[0];
+	        	if(a[1]=="Gerência")
+	        	{
+	        		valorG[0] += b[0];
+	        	}
+	        	if(a[1]=="Vendas")
+	        	{
+	        		valorV[0] += b[0];
+	        	}
+	        	if(a[1]=="Estoque")
+	        	{
+	        		valorE[0] += b[0];
+	        	}
+	        	if(a[1]=="Contabilidade")
+	        	{
+	        		valorC[0] += b[0];
+	        	}
+	        	if(a[1]=="RH")
+	        	{
+	        		valorR[0] += b[0];
+	        	}
+	        	MessageBox.Show("Registro gravado com sucesso!");
+                textBox1.Clear();
+                comboBox1.Text = "";
+                textBox2.Clear();
+                textBox1.Focus();
+        	}
+        	else
+                MessageBox.Show("Preencha todos os campos!");
+        }
+        void Button2Click(object sender, EventArgs e)
+        {
+            string[] dados = richTextBox1.Text.Split('\t');
+            if (comboBox2.SelectedIndex.Equals(0))
+            {
+            	listBox1.Items.Add("Gerência: " + valorG[0]);
+            }
+           if (comboBox2.SelectedIndex.Equals(1))
+            {
+            	listBox1.Items.Add("Vendas: " + valorV[0]);
+            }
+           if (comboBox2.SelectedIndex.Equals(2))
+            {
+            	listBox1.Items.Add("Estoque: " + valorE[0]);
+            }
+           if (comboBox2.SelectedIndex.Equals(3))
+            {
+            	listBox1.Items.Add("Contabilidadde: " + valorC[0]);
+            }
+           if (comboBox2.SelectedIndex.Equals(4))
+            {
+            	listBox1.Items.Add("RH: " + valorR[0]);
+            }
+        }
+        void Button3Click(object sender, EventArgs e)
+        {
+            float soma = 0;
+            for (int i = 0; i < richTextBox1.Lines.Length-1; i++)
+            {
+                string[] dados = richTextBox1.Lines[i].Split('\t');
+                soma += float.Parse(dados[2]);
+            }
+            label4.Text = "Total: " + soma.ToString("R$ #,###,##0,00");
+            float media = soma / (richTextBox1.Lines.Length-1);
+            label5.Text = "Média: " + media.ToString("R$,#,###,##0,00");
+        }
+
+    }
 }
